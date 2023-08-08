de83a215207541afb8bff848a04a72957e064eba
Andre Cocuroci <andremartins@luizalabs.com>
2023-08-08 10:10:27 -0300

    Add model

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:movies/movies_list.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,10 +16,19 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  MoviesList? moviesList;
+
   void _getList() {
-    http.get(Uri.https('api.themoviedb.org', '4/list/1'), headers: {
-      'authorization': 'Bearer a'
-    }).then((value) => print(value.body));
+    String token = '';
+    http.get(Uri.https('api.themoviedb.org', '4/list/1'),
+        headers: {'authorization': 'Bearer $token'}).then((value) {
+      if (value.statusCode == 200) {
+        moviesList = MoviesList.fromJson(jsonDecode(value.body));
+        print(moviesList.toString());
+        setState(() {});
+      }
+      print(value.statusCode);
+    });
   }
 
   @override
@@ -27,10 +39,10 @@ class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Movie: ${moviesList?.name}'),
         ),
       ),
     );
