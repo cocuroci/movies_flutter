3ffc5ecd440ae13f5b841f6303500fae032a8ce3
Andre Cocuroci <andremartins@luizalabs.com>
2023-08-08 09:37:18 -0300

    First request

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  void _getList() {
+    http.get(Uri.https('api.themoviedb.org', '4/list/1'), headers: {
+      'authorization': 'Bearer a'
+    }).then((value) => print(value.body));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getList();
+  }
 
   @override
   Widget build(BuildContext context) {
